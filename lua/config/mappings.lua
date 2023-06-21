e07d2409b171b385e3cc248de2cc0626ef72e855
ES1993 <18981810422@163.com>
2023-06-21 16:05:49 +0800

    temp

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,8 +5,9 @@ maps.n["<leader>g"] = { global.empty, desc = "other" }
 maps.n["<leader>b"] = { global.empty, desc = "buffer" }
 maps.n["<leader>f"] = { global.empty, desc = "find" }
 maps.n["<leader>l"] = { global.empty, desc = "lsp" }
+maps.n["<leader>t"] = { global.empty, desc = "terminal" }
 
-maps.n["<leader>s"] = { "<cmd>w<cr>", desc = "save buffer" }
+maps.n["<leader>w"] = { "<cmd>w<cr>", desc = "save buffer" }
 maps.n["<leader>q"] = { "<cmd>q<cr>", desc = "quit" }
 maps.n["|"] = { "<cmd>vsplit<cr>", desc = "vertical split" }
 maps.n["\\"] = { "<cmd>split<cr>", desc = "horizontal split" }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,44 +1,59 @@
 local global = require("global")
 
+local terminal_env = {
+  https_proxy = "http://127.0.0.1:7890",
+  http_proxy = "http://127.0.0.1:7890",
+  all_proxy = "socks5://127.0.0.1:7890",
+}
+
 global.set_mappings("n", {
-  ["<leader>gt"] = {
+  ["<leader>tt"] = {
     function()
       local terminal = require("toggleterm.terminal")
       terminal.Terminal
           :new({
-            env = {
-              https_proxy = "http://127.0.0.1:7890",
-              http_proxy = "http://127.0.0.1:7890",
-              all_proxy = "socks5://127.0.0.1:7890",
-            },
+            env = terminal_env,
+            direction = "float"
           })
           :toggle()
     end,
-    desc = "open terminal",
+    desc = "open float terminal",
+  },
+  ["<leader>th"] = {
+    function()
+      local terminal = require("toggleterm.terminal")
+      terminal.Terminal
+          :new({
+            env = terminal_env,
+            direction = "horizontal"
+          })
+          :toggle()
+    end,
+    desc = "open horizontal split terminal",
+  },
+  ["<leader>tv"] = {
+    function()
+      local terminal = require("toggleterm.terminal")
+      terminal.Terminal
+          :new({
+            env = terminal_env,
+            direction = "vertical"
+          })
+          :toggle()
+    end,
+    desc = "open vertical split terminal",
   },
   ["<leader>gg"] = {
     function()
       local terminal = require("toggleterm.terminal")
-      terminal.Terminal:new({ cmd = "lazygit" }):toggle()
+      terminal.Terminal:new({
+        cmd = "lazygit",
+        env = terminal_env,
+        direction = "float"
+      }):toggle()
     end,
     desc = "open lazygit",
   },
 })
 
-return {
-  "akinsho/toggleterm.nvim",
-  opts = {
-    size = 10,
-    on_create = function()
-      vim.opt.foldcolumn = "0"
-      vim.opt.signcolumn = "no"
-    end,
-    open_mapping = [[<F7>]],
-    shading_factor = 2,
-    direction = "float",
-    float_opts = {
-      border = "curved",
-      highlights = { border = "Normal", background = "Normal" },
-    },
-  },
-}
+return { "akinsho/toggleterm.nvim" }
